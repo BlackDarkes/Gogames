cf944f035b5664a80167d8f1933f4a15c206645a
DaniilGordeev <gordeev.daniil.andreevic@gmail.com>
2025-07-20 22:33:51 +0500

    Фиксированная шапка

--- src/modules/header/Header/Header.tsx
+++ src/modules/header/Header/Header.tsx
@@ -3,18 +3,35 @@ import IconLogo from "@/assets/images/logo.svg";
 import { Container } from "@/shared/ui/Container/Container";
 import { NavList } from "./components/navList/NavList";
 import { Burger } from "../Burger/Burger";
+import { motion, useMotionValueEvent, useScroll } from "framer-motion";
+import { useState } from "react";
+import { throttle } from "lodash";
 
 import styles from "./Header.module.scss";
 
 export const Header = () => {
+  const [isScrolled, setIsScrolled] = useState<boolean>(false);
+  const { scrollY } = useScroll();
+
+  useMotionValueEvent(
+    scrollY,
+    "change",
+    throttle((latest) => {
+      setIsScrolled(latest > 30);
+    }, 50)
+  );
 
   return (
-    <header className={styles.header}>
+    <motion.header
+      animate={{ backgroundColor: isScrolled ? "#FFE6D7" : "transparent" }}
+      transition={{ duration: 0.4 }}
+      className={styles.header}
+    >
       <Container className={styles.headerContainer}>
         <img src={IconLogo} alt="logo" className={styles.headerLogo} />
         <NavList />
         <Burger />
       </Container>
-    </header>
+    </motion.header>
   );
 };
