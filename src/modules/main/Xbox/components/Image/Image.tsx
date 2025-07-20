a65d4636ae08d793f14c52e753ef8f5c96b970f8
DaniilGordeev <gordeev.daniil.andreevic@gmail.com>
2025-07-20 19:47:41 +0500

    Сделана анимация у Xbox

--- src/modules/main/Xbox/components/Image/Image.tsx
+++ src/modules/main/Xbox/components/Image/Image.tsx
@@ -1,16 +1,38 @@
-import ImageCloud from "../../assets/cloud.svg"
+import ImageCloud from "../../assets/cloud.svg";
 import ImageGearBig from "../../assets/gearBig.svg";
 import ImageGearSmall from "../../assets/gearSmol.svg";
 import ImageGamepad from "../../assets/gamepad.svg";
 import { motion } from "framer-motion";
+import styles from "./Image.module.scss";
 
 export const Image = () => {
   return (
-    <div>
-      <motion.img src={ImageGearSmall} />
-      <motion.img src={ImageGearBig} />
+    <div className={styles.image}>
+      <motion.img
+        src={ImageGearSmall}
+        alt="маленькая шестерёнка"
+        className={`${styles.imageGear} ${styles.imageGearSmall}`}
+        initial={{ rotate: 0 }}
+        animate={{ rotate: 360 }}
+        transition={{ duration: 3, repeat: Infinity, ease: "linear" }}
+      />
+      <motion.img
+        src={ImageGearBig}
+        alt="большая шестерёнка"
+        className={`${styles.imageGear} ${styles.imageGearBig}`}
+        initial={{ rotate: 0 }}
+        animate={{ rotate: 360 }}
+        transition={{ duration: 3, repeat: Infinity, ease: "linear" }}
+      />
       <img src={ImageCloud} alt="облако" />
-      <motion.img src={ImageGamepad} />
+      <motion.img
+        src={ImageGamepad}
+        alt="Контроллер"
+        className={styles.imageGamepad}
+        initial={{ y: 0 }}
+        animate={{ y: [20, -20] }}
+        transition={{ duration: 3, repeat: Infinity, repeatType: "reverse" }}
+      />
     </div>
   );
-}
+};
